4bee0984282088717f22dc900ec97f316e479e43
Sushant <career.sushantchoraghe@gmail.com>
2024-10-14 09:45:01 +0200

    trial 2

--- introduction.cpp
+++ introduction.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-using namespace std;    
+# include <iostream>
+using namespace std;
 
-int main (){
-    cout<<"Hello World"<< endl;
-    return 0;    
+int main  (){
+
+    int a=7;
+    float b=7;
+
+    cout<<a+b;
+    return 0;;
 }
