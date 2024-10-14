4808aed8773481188c2532cdce024109444ac74e
Sushant <career.sushantchoraghe@gmail.com>
2024-10-14 12:13:40 +0200

    Add new learning

---
+++ .vscode/age_checker_for_party.cpp
@@ -0,0 +1,38 @@
+/*
+here we are going to learn 
+1. local, global and constant variables
+2. if else and loops
+3. included own created header file maned as main.hpp
+4. previous learning
+*/
+
+
+# include "main.hpp"
+
+int main(){
+
+    int age;
+
+    cout<<"Enter your completed age"<<endl;
+    cin>>age;
+
+    //ladder if-else statements
+
+    if((age<18) && (age>0)){
+        cout<<"You are not allwed to enter the party"<<endl;
+    }
+
+    else if(age==18){
+        cout<<"You are allowed but bring your ID card"<<endl;
+    }
+
+    else if(age<1){
+        cout<<"Invalid age and not come to my party also"<<endl;
+    }
+
+    else{
+        cout<<"Enjoy the party"<<endl;
+    }
+
+    return 0;
+}
---
+++ .vscode/main.hpp
@@ -0,0 +1,9 @@
+/*
+this s basic header file for practice
+*/
+
+
+# include <iostream>
+# include <string>
+
+using namespace std;
