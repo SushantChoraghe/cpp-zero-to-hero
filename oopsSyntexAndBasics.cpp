39e4f6a9b4bbe1176a171952348ba50841b16d36
Sushant <career.sushantchoraghe@gmail.com>
2024-10-18 17:23:05 +0200

    Add oops learning

---
+++ oopsSyntexAndBasics.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+using namespace std;
+
+
+class Employee {
+
+    private:
+        int a, b, c;
+    
+    public:
+        int e,f;
+};
+
+
+int main(){
+    
+    return 0;
+}
