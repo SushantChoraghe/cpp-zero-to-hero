347774d1e19873c01de69139cd15fbad04957d07
Sushant <career.sushantchoraghe@gmail.com>
2024-10-19 23:31:39 +0200

    Add default and parametric constructor

--- constructor.cpp
+++ constructor.cpp
@@ -22,19 +22,29 @@ class Student {
     int rollno;
     float grade;
 
-
-    Student(string s, int r, float g){ //tis is paramatric constructor and keep the sequesnce same dont change it
+    //paramatric constructor
+    /*
+    Use this parametric constructor with following syntex--> dont change the entry parameter
+    sequence in syntex and maintain same as mentioned in main.
+    */
+    Student(string s, int r, float g){ 
         name = s;
         rollno = r;
         grade = g;
     }
 
-    Student(int r, float g, string s){ //tis is paramatric constructor and keep the sequesnce same dont change it
+    Student(int r, float g, string s){ 
         rollno = r;
         grade = g;
         name = s;
 
     }
+
+    //default constructor
+    /*
+    this is default constructor when we are using objects directly by mentioning 
+    in main file as Student s1,s2,s3 ete
+    */
 
     Student(){ // this is default constructor
 
@@ -61,8 +71,16 @@ void change(Student a){
 
 int main(){
     
+
+    //this is parametric constructor 
+
     Student s1("Himanshu", 98, 9.2);
     Student s2(100,1.8,"Amit");
+    
+
+
+    //this is default constructor
+
     Student s3;
 
     s3.name = "Donald";
@@ -73,14 +91,23 @@ int main(){
     // s2.rollno = 100;
     // s2.grade = 7.8;
 
+
+
     // To print we can make a funtion also which takes the class argument line s1,s2
 
     print(s1);
     print(s2);
     print(s3);
+    
+
+
     //change(s1); //here the s1.name is changes in only function not in main s1. This is called, pass by value 
+    
     //print(s1);
 
+    
+    
+    
     //this is our normal way to show output
     // cout<<s1.name<<" "<<s1.grade<<" "<<s1.rollno<<endl; 
     // cout<<s2.name<<" "<<s2.grade<<" "<<s2.rollno<<endl; 
