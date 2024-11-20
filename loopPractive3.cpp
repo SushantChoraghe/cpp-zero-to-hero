77caba5adf4e1d0790441c44b90f4578d7c80731
Sushant <career.sushantchoraghe@gmail.com>
2024-11-20 16:05:20 +0100

    Loop practice question 3

---
+++ loopPractive3.cpp
@@ -0,0 +1,72 @@
+/*
+Question 1: print "Hero" x times and take x from user
+*/
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//    int x, i;
+//    cout<<"Enter the number for x: "<<endl;
+//    cin>>x;
+
+//     for (i=1; i<=x; i++){
+//         cout<<"Hero"<<endl;
+//     }
+//     return 0;
+// }
+
+
+/*
+Question 2: print number from 1 to 100
+*/
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     for(int i=1; i<=100; i++){
+//         cout<<i<<endl;
+//     }
+
+//     return 0;
+// }
+
+/*
+Question 3: print table of 26
+*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     for(int i=1;i<=10;i++){
+//         cout<<26*i<<endl;
+//     }
+//     return 0;
+// }
+
+
+/*
+Question 4: find AP upto n terms
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i, n;
+    cout<<"Enter the number for n: "<<endl;
+    cin>>n;
+
+    for(i=1; i<=(2n-1);i+=2){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
